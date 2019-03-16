2fef1260ec76d3c1129adff7b2111fac6870dfba
qwjaskzxl <870384605@qq.com>
2019-03-16 20:05:51 +0800

    Update sstream.cpp

--- sstream.cpp
+++ sstream.cpp
@@ -1,1 +1,20 @@
 #include<bits/stdc++.h>
+#include<sstream>
+using namespace std;
+
+void i2s(int x,string &s)
+{
+	stringstream ss;
+	ss<<x;
+	ss>>s;
+}
+int main()
+{
+	int a=999;
+	string b;
+	i2s(a,b);
+	cout<<a<<' '<<b;
+	return 0;
+}
+
+#output:999 999
