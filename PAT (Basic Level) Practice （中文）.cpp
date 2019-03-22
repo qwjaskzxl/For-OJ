3c6f9bcb43a039805d06e1900d736b8c96baa603
qwjaskzxl <870384605@qq.com>
2019-03-22 22:19:35 +0800

    Update PAT (Basic Level) Practice （中文）.cpp

--- PAT (Basic Level) Practice （中文）.cpp
+++ PAT (Basic Level) Practice （中文）.cpp
@@ -1,14 +1,46 @@
+// B1002
+// 读入一个正整数 n，计算其各位数字之和，用汉语拼音写出和的每一位数字。
+// 输入样例：
+// 1234567890987654321123456789
+// 输出样例：
+// yi san wu
+#include<bits/stdc++.h> 
+#include<string> 
+#include<vector>
+using namespace std;
+//map<int,string>mapp;
+vector<string>v;
+vector<string>::iterator it;
+string mapp[10]={"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
+int main()
+{
+	string a;cin>>a;
+	long long s=0;
+	for(int i=0;i<a.length();i++)
+	{
+		s+=a[i]-'0';
+	}	
+	int t=0;
+	while(s>0)
+	{
+		t=s%10;
+		v.push_back(mapp[t]);
+		s/=10;
+	}
+	for(it=v.end()-1;it>v.begin();it--)//或者i=v.size()-1;
+	cout<<*it<<' ';
+	cout<<*v.begin();	
+	return 0;
+}
+
 // B1016
 // 输入样例 1：
 // 3862767 6 13530293 3
 // 输出样例 1：
 // 399
-
 #include <iostream>
 #include <sstream>
- 
-using namespace std;
- 
+using namespace std; 
 int main()
 {
 	long long a, b;
